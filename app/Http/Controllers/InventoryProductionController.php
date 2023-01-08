70836953442dcaf6940867cb0afe3ce5e8fed2c9
marloupepito <marloupepito07@gmail.com>
2023-01-08 16:19:42 +0800

    damd alkdal dwa lkw qowewlak

--- app/Http/Controllers/InventoryProductionController.php
+++ app/Http/Controllers/InventoryProductionController.php
@@ -23,11 +23,11 @@ class InventoryProductionController extends Controller
               ->update(['ingredients_quantity' => $equal]);
           }
 
-              $totalBread = BranchBread::where([['branch_id','=',$request->branchid],['bread_name','=',$request->data[0]['bread_name']]])->first();
-              $result = $totalBread->quantity + $request->data[0]['production_quantity'];
+            //   $totalBread = BranchBread::where([['branch_id','=',$request->branchid],['bread_name','=',$request->data[0]['bread_name']]])->first();
+            //   $result = $totalBread->quantity + $request->data[0]['production_quantity'];
 
-              BranchBread::where('bread_name','=',$request->data[0]['bread_name'])
-              ->update(['quantity' => $result]);
+            //   BranchBread::where('bread_name','=',$request->data[0]['bread_name'])
+            //   ->update(['quantity' => $result]);
 
           return response()->json([
             'status' =>'success'
