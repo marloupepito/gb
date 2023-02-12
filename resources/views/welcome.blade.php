cd7d02c864b2d67257b816c731e5d18069db575a
marloupepito <marloupepito07@gmail.com>
2023-02-12 09:23:44 +0800

    dwkdapwd akwdlakd lawdka

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,12 +15,9 @@
         <!-- Styles -->
         
 
-         @viteReactRefresh
-         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-         
-<!--       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>  -->
+      
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script> 
 
         <style>
             body {
