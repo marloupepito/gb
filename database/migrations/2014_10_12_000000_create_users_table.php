3be4d8739c93533a8d50b710c59b6530626329e6
marloupepito <marloupepito07@gmail.com>
2023-02-27 19:16:54 +0800

    dalk;d ;lakd lka;lkwdklawkdaw

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -55,7 +55,7 @@ class CreateUsersTable extends Migration
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
             $table->string('key')->nullable()->unique();
-            $table->string('branch_name')->nullable()->unique();
+            $table->string('branch_name')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
