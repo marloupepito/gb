0b97dd4c93488f6c548e886497c752d2b806d5e5
Marlou Pepito <marloupepito07@gmail.com>
2023-05-05 01:19:24 +0800

    dadka;lwdklad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>GB Bakeshop</title>
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+      
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
